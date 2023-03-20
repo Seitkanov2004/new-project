a2c679d00347b64543e5a0a1f0840acd77aa01fb
Seitkanov2004 <syrgabek0131@gmail.com>
2023-03-20 20:07:10 +0600

    I did Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,15 +7,17 @@ const Header = () => {
         <div id="header">
             <div className="container">
                 <div className="header">
-                    <h1 className="header--logo">Restaurant</h1>
+                    <h1 className="header--logo">
+                        <h1>Restaurant</h1>
+                    </h1>
                     <div className="header--navbar">
-                        <NavLink to={"/"} >interior</NavLink>
-                        <NavLink to={"/"} >About Us</NavLink>
-                        <NavLink to={"/"} >Menu</NavLink>
-                        <NavLink to={"/"} >Contacts</NavLink>
+                        <NavLink to={"/"}>interior</NavLink>
+                        <NavLink to={"/"}>About Us</NavLink>
+                        <NavLink to={"/"}>Menu</NavLink>
+                        <NavLink to={"/"}>Contacts</NavLink>
                         <div className="header--navbar__search">
                             <input type="text" placeholder="Search"/>
-                            <FiSearch/>
+                            <FiSearch className="header--navbar__search--icon"/>
                         </div>
                         <select>
                             <option>EN</option>
