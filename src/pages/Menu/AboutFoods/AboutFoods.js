675be9c41b567a387f487317eaa173427b420c6e
Seitkanov2004 <114854470+Seitkanov2004@users.noreply.github.com>
2023-03-23 11:07:10 +0600

    I did add new desert (#18)

--- src/pages/Menu/AboutFoods/AboutFoods.js
+++ src/pages/Menu/AboutFoods/AboutFoods.js
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
 import {Foods} from "../../../components/data/foods";
+import {IoIosCloseCircleOutline} from "react-icons/io";
 
 const AboutFoods = () => {
     const [selectedTab, setSelectedTab] = useState(1);
+
+    const [addFoods, setAddFoods] = useState(false)
 
     const handleTabClick = (index) => {
         setSelectedTab(index);
@@ -13,6 +16,9 @@ const AboutFoods = () => {
             <div className="container">
                 <div className="aboutFoods">
                     <div className="aboutFoods--block">
+                        <div style={{
+                            display: addFoods ? "block" : "none"
+                        }} onClick={() => setAddFoods(false)} className="aboutFoods--block__icon"><IoIosCloseCircleOutline/></div>
                         <div className="aboutFoods--block--blockChild">
                             {
                                 Foods.map(el => (
@@ -39,27 +45,96 @@ const AboutFoods = () => {
                                 ))
                             }
                         </div>
-                        <div className="aboutFoods--block--foodsName">
-                            {
-                                Foods.map(el => (
-                                    <div className="aboutFoods--block__onlyFood">
-                                        {
-                                            selectedTab === el.id && el.nameFoots.map(el => (
-                                                <div className="aboutFoods--block__onlyFood__parent">
-                                                    <img src={el.img} alt="" className="aboutFoods--block__onlyFood__parent--img"/>
-                                                    <div className="aboutFoods--block__onlyFood__parent--order">
-                                                        <div className="aboutFoods--block__onlyFood__parent--order__title">
-                                                            <h4>{el.name}</h4>
-                                                            <h5>{el.desert}</h5>
+                        <div>
+                            <div style={{
+                                display: addFoods ? "block" : "none"
+                            }} className="aboutFoods--block__addedFoods">
+                                {
+                                    Foods.slice(0, 1).map(el => (
+                                        <div className="aboutFoods--block__addedFoods--spend">
+                                            {
+                                                el.nameFoots.slice(0, 1).map(el => (
+                                                    <div className="aboutFoods--block__addedFoods--spend__feel">
+                                                        <div
+                                                            className="aboutFoods--block__addedFoods--spend__feel--keep">
+                                                            <div
+                                                                className="aboutFoods--block__addedFoods--spend__feel--keep__make">
+                                                                <img src={el.img} alt=""/>
+                                                                <div
+                                                                    className="aboutFoods--block__addedFoods--spend__feel--keep__make--give">
+                                                                    <div
+                                                                        className="aboutFoods--block__addedFoods--spend__feel--keep__make--give__let">
+                                                                        <h3>{el.name}</h3>
+                                                                        <h5>{el.desert}</h5>
+                                                                    </div>
+                                                                    <h2>{el.price}</h2>
+                                                                </div>
+                                                            </div>
+                                                            <div className="aboutFoods--block__addedFoods--spend__feel--keep__make--block">
+                                                                <div
+                                                                    className="aboutFoods--block__addedFoods--spend__feel--keep--think">
+                                                                    <h3>Extras</h3>
+                                                                    <div
+                                                                        className="aboutFoods--block__addedFoods--spend__feel--keep--think__become">
+                                                                        <h4>Cherry</h4>
+                                                                        <h4>$0.90</h4>
+                                                                    </div>
+                                                                    <div
+                                                                        className="aboutFoods--block__addedFoods--spend__feel--keep--think__become">
+                                                                        <h4>Cherry</h4>
+                                                                        <h4>$0.90</h4>
+                                                                    </div>
+                                                                </div>
+                                                                <div
+                                                                    className="aboutFoods--block__addedFoods--spend__feel--keep--think">
+                                                                    <h3>Extras</h3>
+                                                                    <div
+                                                                        className="aboutFoods--block__addedFoods--spend__feel--keep--think__become">
+                                                                        <h4>Cherry</h4>
+                                                                        <h4>$0.90</h4>
+                                                                    </div>
+                                                                    <div
+                                                                        className="aboutFoods--block__addedFoods--spend__feel--keep--think__become">
+                                                                        <h4>Cherry</h4>
+                                                                        <h4>$0.90</h4>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
                                                         </div>
-                                                        <h3>{el.price}</h3>
                                                     </div>
-                                                </div>
-                                            ))
-                                        }
-                                    </div>
-                                ))
-                            }
+                                                ))
+                                            }
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                            <h1 style={{
+                                display: addFoods ? "block" : "none"
+                            }} className="aboutFoods--block--title">Similar gueries</h1>
+                            <div className="aboutFoods--block--foodsName">
+                                {
+                                    Foods.map(el => (
+                                        <div className="aboutFoods--block__onlyFood">
+                                            {
+                                                selectedTab === el.id && el.nameFoots.map(el => (
+                                                    <div onClick={() => setAddFoods(true)} className="aboutFoods--block__onlyFood__parent">
+                                                        <img src={el.img} alt=""
+                                                             className="aboutFoods--block__onlyFood__parent--img"/>
+                                                        <div className="aboutFoods--block__onlyFood__parent--order">
+                                                            <div
+                                                                className="aboutFoods--block__onlyFood__parent--order__title">
+                                                                <h4>{el.name}</h4>
+                                                                <h5>{el.desert}</h5>
+                                                            </div>
+                                                            <h3>{el.price}</h3>
+                                                        </div>
+                                                    </div>
+                                                ))
+                                            }
+                                        </div>
+                                    ))
+                                }
+                            </div>
                         </div>
                     </div>
                 </div>
