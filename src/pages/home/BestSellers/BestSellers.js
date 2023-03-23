7d2a533dc70e73570c4990bb285673047268be00
Seitkanov2004 <syrgabek0131@gmail.com>
2023-03-23 11:36:14 +0600

    I made slider

--- src/pages/home/BestSellers/BestSellers.js
+++ src/pages/home/BestSellers/BestSellers.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import gou from "../../../assets/img/Frame 10.png"
-import Slider from "react-slick";
 import svg from "../../../assets/img/swiper.png"
 import png from "../../../assets/img/swiper2.png"
+import Slider from "react-slick"
+import next from "./../../../assets/img/next.svg"
 
 const BestSellers = () => {
-
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 2,
+        nextArrow: <img src={next}/>,
+        slidesToScroll: 2,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
 
     return (
         <section id="best">
@@ -25,7 +60,11 @@ const BestSellers = () => {
                         <div style={{
                             display: 'flex'
                         }}>
-                            <Slider >
+                            <Slider {...settings}>
+                                <img src={svg} alt=""/>
+                                <img src={png} alt=""/>
+                                <img src={svg} alt=""/>
+                                <img src={png} alt=""/>
                                 <img src={svg} alt=""/>
                                 <img src={png} alt=""/>
                             </Slider>
