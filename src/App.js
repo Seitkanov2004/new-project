aea7e6ae87fa507edb33300669e6d00cc8dda6c4
Seitkanov2004 <syrgabek0131@gmail.com>
2023-03-26 00:15:36 +0600

    adaptation-menu

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./pages/home/home";
 import Menu from "./pages/Menu/Menu";
 import BurMenu from "./components/Header/burMenu";
+import {useState} from "react";
 
 function App() {
+
+    const [menu, setMenu] = useState(false)
     return (
         <div className="App">
-            <Header/>
-            <BurMenu/>
+            <Header menu={menu} setMenu={setMenu}/>
             <Routes>
                 <Route path={"/"} element={<Home/>}/>
                 <Route path={"/foods"} element={<Menu/>}/>
             </Routes>
+
+            <BurMenu menu={menu}/>
+
             <Footer/>
 
         </div>
